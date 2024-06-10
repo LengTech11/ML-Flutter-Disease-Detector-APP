02ce4bbd0e579a58ea83e879678648908dd451ee
Leng Tech <soklenghoungofficial11@gmail.com>
2024-06-10 12:34:19 +0700

    fix main dart

--- disease_detector_app/lib/main.dart
+++ disease_detector_app/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:disease_detector_app/firebase_helpers/firebase_auth/firebase_auth_helpers.dart';
 import 'package:disease_detector_app/provider/provider.dart';
 import 'package:disease_detector_app/screens/home/home_screen.dart';
@@ -6,11 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'config/themes/theme.dart';
 import 'firebase_options.dart';
 import 'screens/onboarding/onboarding_view.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,14 +16,12 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-
   WidgetsFlutterBinding.ensureInitialized();
   // Stripe.publishableKey =
   // "pk_test_51MWx8OAVMyklfe3CsjEzA1CiiY0XBTlHYbZ8jQlGtVFIwQi4aNeGv8J1HUw4rgSavMTLzTwgn0XRlwoTVRFXyu2h00mRUeWmAf";
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
 
   runApp(
     MultiProvider(
@@ -35,9 +30,10 @@ Future<void> main() async {
         ChangeNotifierProvider(create: (_) => AppProvider()),
         // Add more providers as needed
       ],
-      child: MyApp(),
+      child: const MyApp(),
     ),
-  );}
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -45,31 +41,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return ScreenUtilInit(
-            designSize: const Size(430, 932),
-            minTextAdapt: true,
-            splitScreenMode: true,
-            builder: (context, child) {
-              return MaterialApp(
-                debugShowCheckedModeBanner: false,
-                title: 'VisionCareAI',
-                themeMode: themeProvider.themeMode,
-                theme: AppTheme.lightTheme,
-                darkTheme: AppTheme.darkTheme,
-                home: StreamBuilder(
+      builder: (context, themeProvider, child) {
+        return ScreenUtilInit(
+          designSize: const Size(430, 932),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'VisionCareAI',
+              themeMode: themeProvider.themeMode,
+              theme: AppTheme.lightTheme,
+              darkTheme: AppTheme.darkTheme,
+              home: StreamBuilder(
                   stream: FirebaseAuthHelper.instance.getAuthChange,
                   builder: (context, snapshot) {
-                    if(snapshot.hasData){
-                      return HomeScreen();
+                    if (snapshot.hasData) {
+                      return const HomeScreen();
                     }
-                    return OnboardingView();
-                  }
-                ),
-              );
-            },
-          );
-        },
-      );
+                    return const OnboardingView();
+                  }),
+            );
+          },
+        );
+      },
+    );
   }
 }
