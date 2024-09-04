8a61b2a978accdc2049fdd2f798e8154d5ebc831
hulsambath <hulsambath14@gmail.com>
2024-09-04 21:26:22 +0700

    WIP #247 doctor and clinic api

---
+++ eyes_disease_backend/laravel-app/app/Http/Controllers/Api/ClinicController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ClinicController extends Controller
+{
+    public function index()
+    {
+        // Retrieve all users where user_role is 2 (clinics)
+        $clinics = User::where('user_role', 2)->get();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $clinics,
+        ], 200);
+    }
+
+    public function show($id)
+    {        try {
+        // Retrieve a specific user where user_role is 2
+        $clinic = User::where('user_role', 2)->findOrFail($id);
+        return response()->json([
+            'status' => 'success',
+            'data' => $clinic,
+        ], 200);
+    } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Clinic not found',
+        ], 404);
+    }
+    }
+}
